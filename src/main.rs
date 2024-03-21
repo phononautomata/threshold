1cf58e1af837761d273bdac5c6a89714461236d0
Alfonso de Miguel <alfonso.demiguel.arribas@gmail.com>
2024-03-21 23:32:51 +0100

    experiments renaming

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use clap::Parser;
 use threshold::exps::{
     Args,
-    run_exp1_homogeneous, 
-    run_exp2_datadriven_thresholds,
-    run_exp3_multilayer_thresholds,
+    build_multilayer, 
+    run_epidemic,
+    run_full,
 };
 
 fn main() {
     let input_args = Args::parse();
 
     match input_args.experiment_id {
-        1 => run_exp1_homogeneous(input_args),
-        2 => run_exp2_datadriven_thresholds(input_args),
-        3 => run_exp3_multilayer_thresholds(input_args),
+        1 => build_multilayer(input_args),
+        2 => run_epidemic(input_args),
+        3 => run_full(input_args),
         _ => panic!(),
     };
 }
